428c92e6d7a41395da9b0b06e45a83bb05e39fd9
pdms_ibs_brilliantPC <vjavmvja@gmail.com>
2025-04-01 20:50:39 +0900

    I made a code for color histogram. I stop here to leave office

---
+++ +stm/nest_histcounts.m
@@ -0,0 +1,16 @@
+function results = nest_histcounts(dt,egs,n_loose)
+[N,edges,bin] = histcounts(dt,egs);
+egs_loose = linspace(.5,length(N)+.5,n_loose+1);
+[N_loose,edges_loose,bin_loose] = histcounts(bin,egs_loose);
+[~,~,N_partition] = histcounts(1:length(N),egs_loose);
+results.N = N;
+results.edges = edges;
+results.bin = bin;
+results.N_loose = N_loose;
+results.N_partition = N_partition;
+results.edges_loose = edges_loose;
+results.bin_loose = bin_loose;
+end
+
+%[appendix]{"version":"1.0"}
+%---
